078d54a685e4e2612d04313f053cab2898ea68b7
LongFluffDragon <126533740+LongFluffDragon@users.noreply.github.com>
2024-12-22 19:29:11 -0800

    testing

--- musicplayer-main.js
+++ musicplayer-main.js
@@ -403,7 +403,7 @@
 		setTimeout(function() {
 			main_sc_player_widget.seekTo(30000);
 			main_sc_player_widget.unbind(SC.Widget.Events.PLAY);
-		}, 100);
+		}, 3000);
 		/*
 		if(page_type == "player")
 		{
