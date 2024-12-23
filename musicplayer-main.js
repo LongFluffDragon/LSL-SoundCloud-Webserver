dcb17b7a999abf278b8eb5189043b80ba212e2ac
LongFluffDragon <126533740+LongFluffDragon@users.noreply.github.com>
2024-12-22 17:08:28 -0800

    testing

--- musicplayer-main.js
+++ musicplayer-main.js
@@ -45,7 +45,8 @@
 				if(xhr.status==200)
 				{
 					console.log("xhr response: "+xhr.response);
-					callbackFunction(handle, xhr.response);
+					if(callbackFunction	!= null)
+						callbackFunction(handle, xhr.response);
 				}
 				else
 				{
@@ -499,7 +500,7 @@
 			loaded_track_uri_map.set(id, track_obj);
 			
 			console.log("waveform.height=" + waveform.height + " waveform.length=" + waveform.width + " sps=" + sps + " encoded=" + encode_wf);
-			MakeXHR("", lslServer+"/waveform/" + track_obj.uri, "", encode_wf, "PUT");
+			MakeXHR("", lslServer+"/waveform/" + track_obj.uri, null, encode_wf, "PUT");
 		}
 		// compress and encode keyframes
 		// reduce to 15 bit unicode chars: 5 bit magnitude, 10 bit length
