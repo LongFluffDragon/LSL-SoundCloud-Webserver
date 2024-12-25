6e51505daef2244ab30f21b97fa9d30826b88105
LongFluffDragon <126533740+LongFluffDragon@users.noreply.github.com>
2024-12-24 21:05:38 -0800

    testing

--- musicplayer-main.js
+++ musicplayer-main.js
@@ -641,7 +641,7 @@
 	
 	function getYoutubeId(url)
 	{
-		const regExp = "^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*";
+		const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
 		const match = url.match(regExp);
 
 		return (match && match[2].length === 11) ? match[2] : null;
